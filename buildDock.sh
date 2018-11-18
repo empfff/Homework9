124cf584538c99b3d433b8060fe5c1c54f8a8522
empfff <epfaff@email.unc.edu>
2018-11-18 16:09:20 -0500

    stuff

--- buildDock.sh
+++ buildDock.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 cd py36
+
 docker build -t py36 .
+
+docker run -d \
+  -it \
+  --name py36 \
+  -v "$(pwd)"/output:/app \
+  nginx:latest
---
+++ runPy123.py
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python hw9_1.py 2 3 7 4 6 > hw9_1.txt
